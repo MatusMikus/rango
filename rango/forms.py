df047efd3c42b9b4e3444168ce04a3c9084927a7
suritus <matus.mikus@gmail.com>
2017-02-03 14:28:27 +0000

    chapter 7 done

--- rango/forms.py
+++ rango/forms.py
@@ -1,8 +1,8 @@
 from django import forms 
 from rango.models import Page, Category
 
-class CategoryForm(forms.modelForm):
-	name = forms.CharField(max_length=128,help_text="Please enter the category name.")
+class CategoryForm(forms.ModelForm):
+	name = forms.CharField(max_length=Category.MAXLENGTH,help_text="Please enter the category name.")
 	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 	slug = forms.CharField(widget=forms.HiddenInput(), required = False) # Ppopulated by C.save()
@@ -15,6 +15,16 @@ class PageForm(forms.ModelForm):
 	title = forms.CharField(max_length=128,help_text="Please enter the title of the page")
 	url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
 	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+	
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+		
+		if url and not url.statswith('http://'):
+			url = 'http://' + url
+			cleaned_data['url'] = url
+			return cleaned_data
 
 	class Meta: # Defined so we know which model to provide the form for
 		model = Page
--- rango/models.py
+++ rango/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-	name = models.CharField(max_length=128, unique=True)
+	MAXLENGTH = 128
+	name = models.CharField(max_length=MAXLENGTH, unique=True)
 	views = models.IntegerField(default=0)
 	likes = models.IntegerField(default=0)
 	slug = models.SlugField(unique=True)
